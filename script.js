3b83451d796ec2a5bba352c2a99bd3224625fd56
Peter Ahn <peterahn8@gmail.com>
2023-02-13 14:10:30 -0500

    Use IIFE instead

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-class gameBoard {
-  constructor() {
-    this.controlArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    this.displayArr = ['', '', '', '', '', '', '', '', ''];
-  }
-}
+(function () {
+  const gameBoard = () => {
+    let field = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+    const setVal = (i, val) => {
+      field[i] = val;
+    };
+
+    return { setVal };
+  };
+})();
