b0ad4d4fbdac6600f431faf03f36b99149bdf9ba
Peter Ahn <peterahn8@gmail.com>
2023-02-15 15:46:39 -0500

    Add reset function and button

--- script.js
+++ script.js
@@ -7,7 +7,9 @@
     };
 
     const resetField = () => {
-      field = ['', '', '', '', '', '', '', '', ''];
+      for (let i = 0; i < field.length; i++) {
+        field[i] = '';
+      }
     };
 
     const getField = (index) => {
@@ -41,12 +43,26 @@
       turn++;
     };
 
+    const resetGame = () => {
+      gameBoard.resetField();
+      turn = 1;
+      gameOver = false;
+    };
+
     const getCurrPlayerSign = () => {
       if (turn % 2 === 0) {
         return playerO.getSign();
       } else {
         return playerX.getSign();
       }
+    };
+
+    const checkForTie = () => {
+      if (gameBoard.field.every((val) => val !== '')) {
+        gameOver = true;
+        return true;
+      }
+      return false;
     };
 
     const checkForWinner = () => {
@@ -62,46 +78,75 @@
       ];
 
       for (let c = 0; c < winConditions.length; c++) {
-        if (gameOver) {
-          return;
-        }
         if (winConditions[c].every((val) => gameBoard.getField(val) === 'X')) {
-          console.log('X has won');
           gameOver = true;
-        } else if (
-          winConditions[c].every((val) => gameBoard.getField(val) === 'O')
-        ) {
-          console.log('O has won');
+          return playerX.getSign();
+        }
+
+        if (winConditions[c].every((val) => gameBoard.getField(val) === 'O')) {
           gameOver = true;
+          return playerO.getSign();
         }
       }
     };
 
-    const checkForTie = () => {
-      if (gameBoard.field.every((val) => val !== '')) {
-        gameOver = true;
-        console.log('tie')
-      } 
-    }
-
-    return { playRound };
+    return {
+      playRound,
+      resetGame,
+      getCurrPlayerSign,
+      checkForWinner,
+      checkForTie,
+    };
   })();
 
   const displayController = (() => {
     const _square = document.querySelectorAll('.square');
+    const result = document.querySelector('#result');
+    const resetButton = document.querySelector('#reset');
 
     _square.forEach((square) =>
       square.addEventListener('click', () => {
         if (square.textContent !== '') return;
         gameController.playRound(square.id);
         updateGameBoard();
+        updateResult();
       })
     );
+
+    resetButton.addEventListener('click', () => {
+      gameController.resetGame();
+
+      updateGameBoard();
+      updateResult();
+      console.log(gameBoard.field);
+    });
 
     const updateGameBoard = () => {
       for (let i = 0; i < _square.length; i++) {
         _square[i].textContent = gameBoard.getField(i);
       }
     };
+
+    const updateResult = () => {
+      result.textContent = `Player ${gameController.getCurrPlayerSign()}, make your move!`;
+
+      if (gameController.checkForTie() === true) {
+        result.textContent = `The game was a tie!`;
+      }
+
+      if (gameController.checkForWinner() === 'X') {
+        console.log('X won');
+        result.textContent = `X won!`;
+      }
+
+      if (gameController.checkForWinner() === 'O') {
+        console.log('O won');
+        result.textContent = `O won!`;
+      }
+    };
+
+    updateResult();
+
+    return { updateResult };
   })();
 })();
