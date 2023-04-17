3dc170087281adf53ec0eceb394d14a503d6cfea
Peter Ahn <peterahn8@gmail.com>
2023-04-17 12:42:27 -0400

    Implement animations and add comments.
    Also fix tons of broken functions. Removed redundant function calls. Added some ChatGPT quips to make it more interactive

--- script.js
+++ script.js
@@ -10,6 +10,7 @@
       for (let i = 0; i < field.length; i++) {
         field[i] = i;
       }
+      console.log(`Field was reset to: [${field}].`);
     };
 
     const getFieldIndex = (index) => {
@@ -32,26 +33,30 @@
   // Manage the game logic
   const gameController = (() => {
     let turn = 1;
-    let gameOver = false;
+    let losses = 0;
 
     const playRound = (val) => {
-      console.log(gameOver);
+      // Prevent `minimaxLogic` from traversing [moves] needlessly on the last round
+      if (turn === 10) {
+        return false;
+      }
+
       if (getCurrPlayer() === minimax) {
         turn++;
         const index = minimaxLogic(gameField.field, minimax).index;
         gameField.field[index] = minimax;
-        console.log(`Minimax has decided. It is now turn: ` + turn);
+        console.log(`Minimax decided. Current turn is ${turn}.`);
+        console.log(`Waiting for human...`);
       } else {
         gameField.field[val] = human;
         turn++;
-        console.log(`Human has decided. It is now turn: ` + turn);
+        console.log(`Human decided. Current turn is ${turn}.`);
       }
     };
 
     const resetGame = () => {
       gameField.resetField();
       turn = 1;
-      gameOver = false;
     };
 
     const getCurrPlayer = () => {
@@ -61,15 +66,21 @@
       return human;
     };
 
-    const checkForTie = () => {
+    const checkTie = () => {
       if (
         gameField.field.every(
-          (index) => (index === human || index === minimax) && !checkWinStates()
+          (index) => (index === human || index === minimax) && !checkWin()
         )
       ) {
-        gameOver = true;
         return true;
       }
+    };
+
+    const checkLoss = () => {
+      if (checkTie() || checkWin(human) || checkWin(minimax)) {
+        return true;
+      }
+      return false;
     };
 
     // Control the AI player
@@ -77,9 +88,10 @@
       // Only look at available `field` indexes to reduce time complexity
       const availIndex = gameField.getEmptyFieldIndexes();
 
-      if (checkWinStates(human)) {
+      // Recursively rank terminal states
+      if (checkWin(human)) {
         return { score: -10 };
-      } else if (checkWinStates(minimax)) {
+      } else if (checkWin(minimax)) {
         return { score: 10 };
       } else if (availIndex.length === 0) {
         return { score: 0 };
@@ -87,6 +99,7 @@
 
       const moves = [];
 
+      // Recursively loop through possible moves, based on available squares
       for (let i = 0; i < availIndex.length; i++) {
         const move = {};
         move.index = newField[availIndex[i]];
@@ -110,7 +123,7 @@
 
       let bestMove;
 
-      // Count up from -10000 or count down from 10000, depending on the current player
+      // Count up from -10000 or count down from 10000, depending on current player
       if (player === minimax) {
         let bestScore = -10000;
         for (let i = 0; i < moves.length; i++) {
@@ -129,10 +142,11 @@
         }
       }
 
+      // Return the best scoring move
       return moves[bestMove];
     };
 
-    const checkWinStates = (player) => {
+    const checkWin = (player) => {
       const field = gameField.field;
 
       if (
@@ -145,7 +159,6 @@
         (field[0] === player && field[4] === player && field[8] === player) ||
         (field[2] === player && field[4] === player && field[6] === player)
       ) {
-        gameOver = true;
         return true;
       }
     };
@@ -154,9 +167,10 @@
       playRound,
       resetGame,
       getCurrPlayer,
-      checkForTie,
+      checkTie,
       minimaxLogic,
-      checkWinStates,
+      checkWin,
+      checkLoss,
       turn,
     };
   })();
@@ -164,17 +178,17 @@
   // Display the game board and handle user input
   const displayController = (() => {
     const _square = document.querySelectorAll('.square');
-    const result = document.querySelector('#result');
-    const resetButton = document.querySelector('#reset');
+    const _buttonByID = document.querySelectorAll('button.square');
+    const _display = document.querySelector('#display');
+    const _result = document.querySelector('#result');
+    const _resetButton = document.querySelector('#reset');
 
     _square.forEach((square) =>
       square.addEventListener('click', () => {
         if (
           gameField.field[square.id] === human ||
           gameField.field[square.id] === minimax ||
-          gameController.checkForTie() ||
-          gameController.checkWinStates(human) ||
-          gameController.checkWinStates(minimax)
+          gameController.checkLoss()
         ) {
           return false;
         }
@@ -182,24 +196,30 @@
         if (gameController.getCurrPlayer() === minimax) {
           gameController.playRound(square.id);
         }
-        updateDisplay();
+        updateSquares();
         updateResult();
+        if (gameController.checkLoss()) {
+          highlight();
+        }
       })
     );
 
-    resetButton.addEventListener('click', () => {
+    _resetButton.addEventListener('click', () => {
       gameController.resetGame();
       updateResult();
-      updateDisplay();
-      console.log(gameField.field);
+      updateSquares();
+      resetClasses();
     });
 
-    const updateDisplay = () => {
+    const updateSquares = () => {
       for (let i = 0; i < _square.length; i++) {
         if (
           gameField.getFieldIndex(i) === human ||
           gameField.getFieldIndex(i) === minimax
         ) {
+          if (gameField.getFieldIndex(i) === minimax) {
+            _square[i].classList.add('squareFade');
+          }
           _square[i].textContent = gameField.getFieldIndex(i);
         } else {
           _square[i].textContent = '';
@@ -208,21 +228,113 @@
     };
 
     const updateResult = () => {
-      result.textContent = `Make your move!`;
+      _result.textContent = `Show me your moves!`;
 
-      if (gameController.checkForTie() === true) {
-        result.textContent = `The game was a tie!`;
-      } else if (gameController.checkWinStates(minimax)) {
-        result.textContent = `Minimax wins!`;
-      } else if (gameController.checkWinStates(human)) {
-        result.textContent = `You won! Wait, that's impossible.`;
+      if (gameController.checkTie()) {
+        fadeResult();
+        _result.textContent = getAnnoyed();
+      } else if (gameController.checkWin(minimax)) {
+        fadeResult();
+        _result.textContent = getAnnoyed();
+      } else if (gameController.checkWin(human)) {
+        fadeResult();
+        _result.textContent = `You won! Wait, that's impossible.`;
       }
     };
 
-    return { updateResult, updateDisplay };
+    // Return quips depending on loss or tie
+    // These are AI-generated strings, courtesy of Chat GPT-3
+    const getAnnoyed = () => {
+      const loseArray = [
+        `Nice move! But I can see a few more steps ahead, can you keep up?`,
+        `Looks like you're catching on! Don't worry, I'll try to make it interesting.`,
+        `You're doing great! It's almost like you've got a little bit of Minimax in you.`,
+        `Well played! It's always fun to have a challenge.`,
+        `You're getting closer to beating me! But don't get too confident, I still have a few tricks up my sleeve.`,
+        `You're putting up a good fight, but I'm programmed to win. Sorry!`,
+        `Looks like I've got the upper hand. Don't worry, practice makes perfect!`,
+        `I'm seeing some patterns in your moves. Don't worry, I won't tell anyone ;)`,
+      ];
+
+      const tieArray = [
+        'You managed to hold your own against me. Impressive!',
+        `Looks like we're evenly matched. Let's play again soon!`,
+        'A tie? That just means we need a rematch to settle the score!',
+        `We're both winners today! ...Sort of.`,
+        `Well played! It's not easy to tie with me, you know.`,
+        `A tie? I thought I had you there for a second. Good job holding your ground!`,
+        `Well, a tie isn't a loss, but it's not a win either. Let's try again and see who comes out on top.`,
+        `Well played, my friend. I think we've both earned the right to call ourselves average Tic-Tac-Toe players.`,
+        `A tie? That's almost as frustrating as a loss. I guess we'll just have to keep playing until someone wins.`,
+      ];
+
+      if (gameController.checkWin(minimax)) {
+        return loseArray[Math.floor(Math.random() * loseArray.length)];
+      } else if (gameController.checkTie()) {
+        return tieArray[Math.floor(Math.random() * tieArray.length)];
+      }
+    };
+
+    const highlight = () => {
+      const combinations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+      ];
+
+      for (let i = 0; i < combinations.length; i++) {
+        if (
+          combinations[i].every(
+            (val) => gameField.getFieldIndex(val) === minimax
+          )
+        ) {
+          combinations[i].forEach((val) =>
+            _buttonByID[val].classList.add('loseHighlight')
+          );
+        }
+      }
+
+      if (gameController.checkTie()) {
+        _buttonByID.forEach((val) => {
+          val.classList.add('tieHighlight');
+        });
+      }
+    };
+
+    const fadeDisplay = () => {
+      _display.classList.add('displayFade');
+      _resetButton.classList.add('displayFade');
+
+      setTimeout(() => {
+        _display.classList.remove('displayFade');
+        _resetButton.classList.remove('displayFade');
+      }, 800);
+    };
+
+    const fadeResult = () => {
+      _result.classList.add('resultFade');
+
+      setTimeout(() => {
+        _result.classList.remove('resultFade');
+      }, 400);
+    };
+
+    const resetClasses = () => {
+      _buttonByID.forEach((val) => {
+        val.classList.remove('loseHighlight', 'tieHighlight', 'squareFade');
+      });
+      fadeDisplay();
+      fadeResult();
+    };
+
+    return { updateResult };
   })();
 
-  // Initialize the display on screen
+  // Initialize the result readout
   displayController.updateResult();
-  console.log(`Waiting for human. It is now turn: ` + gameController.turn);
 })();
